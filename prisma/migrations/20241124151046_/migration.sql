732e6cda63c039be6900c5f9976e1bd9c4957835
yuu19 <yusuke.kusi1028@gmail.com>
2025-03-23 22:21:04 +0900

    remove unused files

--- prisma/migrations/20241124151046_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "Comment_postId_idx" ON "Comment"("postId");
-
--- CreateIndex
-CREATE INDEX "Comment_parentId_idx" ON "Comment"("parentId");
-
--- AddForeignKey
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
